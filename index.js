7d1376948bdc3364d0d03f76449399def30ba7c6
ruwiniw <ruwini.wijesekera@gmail.com>
2023-01-14 22:06:20 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,80 @@
+const mongoose = require('mongoose');
+
+const Dishes = require('./models/dishes');
+
+const url = 'mongodb://localhost:27017/conFusion';
+
+const connect = mongoose.connect(url);
+
+connect.then((db) => {
+    console.log('Connected correctly to server');
+
+    Dishes.create({
+        name: 'Pizza',
+        description: 'test'
+    })
+    .then((dish) => {
+        console.log(dish);
+        return Dishes.findByIdAndUpdate(dish._id, {
+            $set: { description: 'Updated test'}
+        },{
+            new: true
+        })
+        .exec();
+    })
+    .then((dish) => {
+        console.log(dish);
+
+        dish.comments.push({ 
+            rating: 5,
+            comment: 'I am getting sinking feeling',
+            author: 'baby'
+        });
+        return dish.save();
+    })
+    .then((dish) => {
+        console.log(dish);
+        return Dishes.remove({});
+    })
+    .then(() => {
+        return mongoose.connection.close();
+    })
+    .catch((err) => {
+        console.log(err);
+    });
+});
+
+
+
+// const mongoose = require('mongoose');
+
+// const Dishes = require('./models/dishes');
+
+// const url = 'mongodb://localhost:27017/conFusion';
+
+// const connect = mongoose.connect(url);
+
+// connect.then((db) => {
+//     console.log('Connected correctly to server');
+
+//     var newDish = Dishes({
+//         name: 'Pizza',
+//         description: 'test'
+//     });
+
+//     newDish.save()
+//         .then((dish) => {
+//             console.log(dish);
+//             return Dishes.find({});
+//         })
+//         .then((dishes) => {
+//             console.log(dishes);
+//             return Dishes.remove({})
+//         })
+//         .then(() => {
+//             return mongoose.connection.close();
+//         })
+//         .catch((err) => {
+//             console.log(err);
+//         });
+// });
